9a2d2c2f37e9fdecbc8ed8692a6cb581681c927c
phaetton <31552767+phaetton@users.noreply.github.com>
2022-09-02 11:57:28 -0600

    creando controladores
    reduciendo codigo del web

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,86 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('home');
+    }
+
+    //categorias
+    public function categorias()
+    {
+        //consulta a la base de datos
+        $posts = [
+            ['Id_Categoria' => 1, 'Nombre' => 'comida', 'Descripcion' => 'Venta de comida', 'slug' => "food"],
+            ['Id_Categoria' => 2, 'Nombre' => 'veterinaria', 'Descripcion' => 'cuidado animal', 'slug' => "vet"],
+        ];
+        return view('categorias', ['posts' => $posts]);
+    }
+
+    public function categoria($slug)
+    {
+        // consulta a base de datos
+        $post = $slug;
+        return view('categoria', ['post' => $post]);
+    }
+
+    //negocios
+    public function negocios()
+    {
+        //consulta a la base de datos
+        $posts = [
+            ['Id_Negocio' => 1, 'Nombre' => 'Comedor Rosita', 'Descripcion' => 'Venta de comida en su propio local', 'Id_Categoria' => 1, 'NombreCategoria' => 'comida', 'Id_TipoNegocio' => 2, 'NombreTipoNegocio' => 'Productos', 'Activo' => true],
+            ['Id_Negocio' => 2, 'Nombre' => 'Pizza a domicilio', 'Descripcion' => 'Venta de comida a domicilio', 'Id_Categoria' => 1, 'NombreCategoria' => 'comida', 'Id_TipoNegocio' => 2, 'NombreTipoNegocio' => 'Productos', 'Activo' => true],
+        ];
+        return view('negocios', ['posts' => $posts]);
+    }
+
+    public function negocio($id)
+    {
+        // consulta a base de datos
+        $post = $id;
+        return view('negocio', ['post' => $post]);
+    }
+
+
+    //tipo negocios
+    public function tiponegocios()
+    {
+        return view('listado de tipos de negocios');
+    }
+
+    public function tiponegocio($id)
+    {
+        // consulta a base de datos
+        $post = $id;
+        return view('tiponegocio', ['post' => $post]);
+    }
+
+
+    //usuarios
+    public function usuarios()
+    {
+        return view('listado de usuarios');
+    }
+
+    public function usuario($id)
+    {
+        // consulta a base de datos
+        $post = $id;
+        return view('usuarios', ['post' => $post]);
+    }
+
+
+    //clientes
+    public function clientes()
+    {
+        return view('listado de clientes');
+    }
+
+    public function cliente($id)
+    {
+        // consulta a base de datos
+        $post = $id;
+        return view('cliente', ['post' => $post]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,92 +13,40 @@ use Illuminate\Support\Facades\Route;
 * las vistas estan en resources/views
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [PageController::class,'home'])->name('home');
 
 //categorias
-Route::get('categorias', function () {
-    //consulta a la base de datos
-    $posts = [
-        ['Id_Categoria' => 1, 'Nombre' => 'comida', 'Descripcion' => 'Venta de comida', 'slug' => "food"],
-        ['Id_Categoria' => 2, 'Nombre' => 'veterinaria', 'Descripcion' => 'cuidado animal', 'slug' => "vet"],
-    ];
-    return view('categorias', ['posts' => $posts]);
-})->name('categorias');
-
-Route::get('categoria/{slug}', function ($slug) {
-    // consulta a base de datos
-    $post = $slug;
-    return view('categoria', ['post' => $post]);
-})->name('categoria');
-
+Route::get('categorias',[PageController::class,'categorias'])->name('categorias');
+Route::get('categoria/{slug}', [PageController::class,'categoria'])->name('categoria');
 Route::get('categorias/buscar', function (Request $request) {
     return $request->all();
 });
 
 
 //negocios
-Route::get('negocios', function () {
-    //consulta a la base de datos
-    $posts = [
-        ['Id_Negocio' => 1, 'Nombre' => 'Comedor Rosita', 'Descripcion' => 'Venta de comida en su propio local', 'Id_Categoria' => 1, 'NombreCategoria' => 'comida', 'Id_TipoNegocio' => 2, 'NombreTipoNegocio' => 'Productos', 'Activo' => true],
-        ['Id_Negocio' => 2, 'Nombre' => 'Pizza a domicilio', 'Descripcion' => 'Venta de comida a domicilio', 'Id_Categoria' => 1, 'NombreCategoria' => 'comida', 'Id_TipoNegocio' => 2, 'NombreTipoNegocio' => 'Productos', 'Activo' => true],
-    ];
-    return view('negocios', ['posts' => $posts]);
-})->name('negocios');
-
-Route::get('negocio/{id}', function ($id) {
-    // consulta a base de datos
-    $post = $id;
-    return view('negocio', ['post' => $post]);
-})->name('negocio');
-
+Route::get('negocios',[PageController::class,'negocios'])->name('negocios');
+Route::get('negocio/{id}', [PageController::class,'negocio'])->name('negocio');
 Route::get('negocios/buscar', function (Request $request) {
     return $request->all();
 });
 
 //tipo negocios
-Route::get('tiponegocios', function () {
-    return view('listado de tipos de negocios');
-})->name('tiponegocios');
-
-Route::get('tiponegocio/{id}', function ($id) {
-    // consulta a base de datos
-    $post = $id;
-    return view('tiponegocio', ['post' => $post]);
-})->name('tiponegocio');
-
+Route::get('tiponegocios', [PageController::class,'tiponegocios'])->name('tiponegocios');
+Route::get('tiponegocio/{id}', [PageController::class,'tiponegocio'])->name('tiponegocio');
 Route::get('tiponegocios/buscar', function (Request $request) {
     return $request->all();
 });
 
 //usuarios
-Route::get('usuarios', function () {
-    return view('listado de usuarios');
-})->name('usuarios');
-
-Route::get('usuario/{id}', function ($id) {
-    // consulta a base de datos
-    $post = $id;
-    return view('usuarios', ['post' => $post]);
-})->name('usuario');
-
+Route::get('usuarios', [PageController::class,'usuarios'])->name('usuarios');
+Route::get('usuario/{id}', [PageController::class,'usuario'])->name('usuario');
 Route::get('usuarios/buscar', function (Request $request) {
     return $request->all();
 });
 
 //clientes
-Route::get('clientes', function () {
-    return view('listado de clientes');
-})->name('clientes');
-
-Route::get('cliente/{id}', function ($id) {
-    // consulta a base de datos
-    $post = $id;
-    return view('cliente', ['post' => $post]);
-})->name('cliente');
-
+Route::get('clientes', [PageController::class,'clientes'])->name('clientes');
+Route::get('cliente/{id}', [PageController::class,'cliente'])->name('cliente');
 Route::get('clientes/buscar', function (Request $request) {
     return $request->all();
 });
