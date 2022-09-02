861b0cda60538eae79cc437f955ec0fd8d516ced
phaetton <31552767+phaetton@users.noreply.github.com>
2022-09-02 10:59:59 -0600

    creando vistas

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -5,16 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>El biyuyo</title>
+    <title>Document</title>
 </head>
 
 <body>
-    <h1>Listado de Categorias</h1>
-
-    @foreach($posts as $post)
-    <p>{{$post['Id_Categoria']}} <a href="#">{{$post['Nombre']}} {{$post['Descripcion']}}</a> </p>
-
-    @endforeach
+    <h1>Detalle</h1>
+    {{$post}}
 </body>
 
 </html>
