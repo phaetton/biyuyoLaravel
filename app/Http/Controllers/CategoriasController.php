dc9a604ba697953b4804fbd197e1c721988693c1
phaetton <31552767+phaetton@users.noreply.github.com>
2022-09-05 16:44:26 -0600

    vista de administrador: categorias

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorias;
 use Illuminate\Http\Request;
 
 class CategoriasController extends Controller
 {
     public function index()
     {
-        return view('categorias.index');
+        return view('categorias.index',[
+            'categorias'=>Categorias::latest()->paginate()
+        ]);
     }
 }
