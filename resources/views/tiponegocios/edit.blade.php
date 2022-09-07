714b325f07b108b651ec4d64cb738bd45bda1762
phaetton <31552767+phaetton@users.noreply.github.com>
2022-09-07 15:50:28 -0600

    finiquitando primera versión

--- resources/views/tiponegocios/edit.blade.php
+++ resources/views/tiponegocios/edit.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Tipo de negocios') }}
-            <a href="{{route('categorias.create')}}" 
-            class="text-xs bg-gray-800 text-white rounded px-2 py-1">Crear</a>
+            {{ __('Tipo de negocios') }}          
         </h2>
     </x-slot>
 
