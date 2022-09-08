e1474906dfd4bd9c17a5cbc964031216f678c178
phaetton <31552767+phaetton@users.noreply.github.com>
2022-09-08 09:48:55 -0600

    agregando fotos al negocio y al cliente

--- database/factories/NegociosFactory.php
+++ database/factories/NegociosFactory.php
@@ -20,6 +20,7 @@ class NegociosFactory extends Factory
     {
         return [
             'user_id'           => 1,
+            'logo'              => 'https://source.unsplash.com/random',
             'nombre'            => $nombre = $this->faker->sentence(),
             'descripcion'       => $this->faker->text(2200),
             'slug'              => Str::slug($nombre),
